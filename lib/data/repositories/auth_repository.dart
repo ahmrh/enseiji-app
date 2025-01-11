554e65273916d2a6eef67e1b4c39d48663919508
ahmrh <ahmadrolandi01@gmail.com>
2025-01-11 15:27:15 +0700

    refactor: restructuring the project

---
+++ lib/data/repositories/auth_repository.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+class AuthRepository extends ChangeNotifier {
+  
+}
---
+++ lib/ui/screens/landing/landing_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+
+  }
+}
